aa42a88c80b5e7d10c2ff46fa34a5318d79df4c6
nabe16 <tarna0116@gmail.com>
2020-07-09 01:38:21 +0900

    update index.php

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,3 +1,52 @@
-<?php
-    phpinfo();
-?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+<title>Login</title>
+<style>
+body {
+    background-color: #f5f5f5;
+}
+h1 {
+    text-align: center;
+    font-size: 200px;
+    color: #d3d3d3;
+}
+#user , #input{
+    font-size: 50px;
+    color: #d3d3d3;
+}
+
+#input {
+    margin: 50px 200px 50px 200px;
+}
+
+</style>
+
+</head>
+
+<body>
+<h1>Login</h1>
+
+<div id="input">
+<form>
+<div class="form-group">
+    <label for="inputUserId" id="user">UserID</label>
+    <input type="userid" class="form-control" id="inputUserId" aria-describedby="userHelp" placeholder="Enter UserID">
+</div>
+<div class="form-group">
+    <label for="inputPassword">Password</label>
+    <input type="password" class="form-control" id="inputPassword" placeholder="Password">
+</div>
+</form>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+</body>
+
+</html>
+
